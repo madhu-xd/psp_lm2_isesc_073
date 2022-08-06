0e3a9dd7ed4a27e9fb7fce7b8a860b63891b24c4
madhu-xd <110719339+madhu-xd@users.noreply.github.com>
2022-08-06 16:36:37 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -1,1 +1,13 @@
 // addition of two numbers
+
+# include <stdio.h>
+# include <math.h>
+int main()
+{
+  int a,b,sum;
+  printf("enter the two numbers : ");
+  scanf("%d%d",&a,&b);
+  sum = a+b;
+  printf("the sum of two numbers %d and %d =%d",a,b,sum);
+  return 0;
+}
