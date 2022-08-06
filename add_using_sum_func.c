1124b87b02c1fea696cbc8394939e4f51a77eefb
madhu-xd <110719339+madhu-xd@users.noreply.github.com>
2022-08-06 17:29:11 +0530

    Update add_using_sum_func.c

--- add_using_sum_func.c
+++ add_using_sum_func.c
@@ -1,1 +1,17 @@
+# include <stdio.h>
+# include <math.h>
 
+int add(int a,int b,int c,int d)
+{
+  int sum = a+b+c+d;
+  return sum;
+}
+int main()
+{
+  int a,b,c,d,sum;
+  printf("input four numbers for addition: ");
+  scanf("%d%d%d%d",&a,&b,&c,&d);
+  sum = add(a,b,c,d)
+  printf("the sum of four numbers is %d+ %d + %d +%d =%d",a,b,c,d,sum);
+  return 0;
+}
